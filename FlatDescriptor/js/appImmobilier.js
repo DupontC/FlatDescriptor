42cf5373f464e20b1e478763d026032e77df410d
Cyril Dupont <cyril.dupont61@gmail.com>
2014-10-15 23:46:52 +0200

    routage vers la page d'index avec les données en statique

--- FlatDescriptor/js/appImmobilier.js
+++ FlatDescriptor/js/appImmobilier.js
@@ -6,18 +6,74 @@ FlatApp.controller('FlatFrontOfficeCtrl',function($scope, $http, $location) {
 
     $http({method: 'GET', url: './immo.json'}).
       success(function(data, status, headers, config) {
-        var url = $location.absUrl();
-        var param = url.split("?");
-        var id = param[1].split("=");
 
-        if(null != data && null != id){
-          if(isNumeric(id[1]) && id[1] <= data.length-1 && data[id[1]]["enLigne"] == true){
-            $scope.appartement = data[id[1]];
+        data = [
+        {
+            "enLigne" : false,
+            "ville" : "Caen",
+            "titre1" : "Lemon drops",
+            "texte1" : "Fruitcake toffee jujubes. Topping biscuit sesame snaps jelly caramels jujubes tiramisu fruitcake. Marzipan tart lemon drops chocolate sesame snaps jelly beans.",
+            "image1": "../images/1.png",
+            "titre2" : "Plum caramels",
+            "texte2" : "Lollipop powder danish sugar plum caramels liquorice sweet cookie. Gummi bears caramels gummi bears candy canes cheesecake sweet roll icing dragée. Gummies jelly-o tart. Cheesecake unerdwear.com candy canes apple pie halvah chocolate tiramisu.",
+            "image2" : "../images/2.png",
+            "titre3" : "Marzipan gingerbread",
+            "texte3" : "Soufflé bonbon jelly cotton candy liquorice dessert jelly bear claw candy canes. Pudding halvah bonbon marzipan powder. Marzipan gingerbread sweet jelly.",
+            "image3" : "../images/3.png",
+            "titre4" : "Carrot cake",
+            "texte4" : "Sesame snaps sweet wafer danish. Chupa chups carrot cake icing donut halvah bonbon. Chocolate cake candy marshmallow pudding dessert marzipan jujubes sugar plum.",
+            "image4" : "../images/4.png",
+            "emplacement":"Sesame snaps sweet wafer danish. Chupa chups carrot cake icing donut halvah bonbon. Chocolate cake candy marshmallow pudding dessert marzipan jujubes sugar plum.",
+            "latitude" : "45.7593",
+            "longitude" : "4.8431",
+            "adresse" : "15 rue de la Part-Dieu 69003 LYON",
+            "photos":"Cake cotton candy lollipop. Cake croissant cheesecake candy sugar plum icing apple pie wafer. Pie sugar plum tiramisu tiramisu pie fruitcake candy icing. Candy icing gummies gummies cheesecake brownie lemon drops chocolate gingerbread.",
+            "albumPhotos" : "https://www.flickr.com/photos/125729062@N07/14522173061/player/3931c36915",
+            "contacts":"Cake cotton candy lollipop. Cake croissant cheesecake candy sugar plum icing apple pie wafer. Pie sugar plum tiramisu tiramisu pie fruitcake candy icing. Candy icing gummies gummies cheesecake brownie lemon drops chocolate gingerbread."
+          },
+          {
+            "enLigne" : true,
+            "ville" : "Caen",
+            "localisation" : "15 rue de la Part-Dieu",
+            "titre1" : "Lemon drops",
+            "texte1" : "Fruitcake toffee jujubes. Topping biscuit sesame snaps jelly caramels jujubes tiramisu fruitcake. Marzipan tart lemon drops chocolate sesame snaps jelly beans.",
+            "image1": "../images/1.png",
+            "titre2" : "Plum caramels",
+            "texte2" : "Lollipop powder danish sugar plum caramels liquorice sweet cookie. Gummi bears caramels gummi bears candy canes cheesecake sweet roll icing dragée. Gummies jelly-o tart. Cheesecake unerdwear.com candy canes apple pie halvah chocolate tiramisu.",
+            "image2" : "../images/2.png",
+            "titre3" : "Marzipan gingerbread",
+            "texte3" : "Soufflé bonbon jelly cotton candy liquorice dessert jelly bear claw candy canes. Pudding halvah bonbon marzipan powder. Marzipan gingerbread sweet jelly.",
+            "image3" : "../images/3.png",
+            "titre4" : "Carrot cake",
+            "texte4" : "Sesame snaps sweet wafer danish. Chupa chups carrot cake icing donut halvah bonbon. Chocolate cake candy marshmallow pudding dessert marzipan jujubes sugar plum.",
+            "image4" : "../images/4.png",
+            "titre5" : "Pudding lollipop",
+            "texte5" : "Chupa chups pudding lollipop gummi bears gummies cupcake pie. Cookie cotton candy caramels. Oat cake dessert applicake. Sweet roll tiramisu sweet roll sweet roll.",
+            "image5" : "../images/5.png",
+            "titre6" : "Soufflé bonbon",
+            "texte6" : "Cake cotton candy lollipop. Cake croissant cheesecake candy sugar plum icing apple pie wafer. Pie sugar plum tiramisu tiramisu pie fruitcake candy icing. Candy icing gummies gummies cheesecake brownie lemon drops chocolate gingerbread.",
+            "image6" : "../images/6.png",
+            "emplacement":"Sesame snaps sweet wafer danish. Chupa chups carrot cake icing donut halvah bonbon. Chocolate cake candy marshmallow pudding dessert marzipan jujubes sugar plum.",
+            "latitude" : "45.7593",
+            "longitude" : "4.8431",
+            "photos":"Cake cotton candy lollipop. Cake croissant cheesecake candy sugar plum icing apple pie wafer. Pie sugar plum tiramisu tiramisu pie fruitcake candy icing. Candy icing gummies gummies cheesecake brownie lemon drops chocolate gingerbread.",
+            "albumPhotos" : "https://www.flickr.com/photos/125729062@N07/14522173061/player/3931c36915",
+            "contacts":"Cake cotton candy lollipop. Cake croissant cheesecake candy sugar plum icing apple pie wafer. Pie sugar plum tiramisu tiramisu pie fruitcake candy icing. Candy icing gummies gummies cheesecake brownie lemon drops chocolate gingerbread."
+          }
+
+          ]
+
+        var url = $location.absUrl();
+        var param = url.split("/");
+        var indiceID = param[param.length-1];
+        if(null != data && null != indiceID){
+         if(isNumeric(indiceID) && indiceID <= data.length-1 && data[indiceID]["enLigne"] == true){
+            $scope.appartement = data[indiceID];
             latitude = $scope.appartement.latitude;
             longitude = $scope.appartement.longitude;
             urlAlbum = $scope.appartement.albumPhotos;
           }else{
-            alert("Désolé ,annonce non disponible");
+            //alert("Désolé ,annonce non disponible");
           }
         }
       }).
@@ -30,8 +86,11 @@ function isNumeric(obj) {
     return !isNaN(parseFloat(obj)) && isFinite(obj);
 }
 
-//variables pour la mise en place des variable pour la direction
+/**
+*Initilisation de la cartographie et du carousel
+**/
 function initialize() {
+  //variables pour la mise en place des variable pour la direction
   if(null != longitude && null != latitude && null != urlAlbum){
 
     //on place l'adresse url de notre album photo
@@ -69,12 +128,15 @@ function initialize() {
   }
 }
 
-  function loadScript() {
-      var script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
-      'callback=initialize';
-      document.body.appendChild(script);
-  }
+/**
+* Fonction de chargement de l'api Google Map
+**/
+function loadScript() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
+    'callback=initialize';
+    document.body.appendChild(script);
+}
 
-  window.onload = loadScript;
+window.onload = loadScript;
--- FlatDescriptor/serveurNodeJS.js
+++ FlatDescriptor/serveurNodeJS.js
@@ -1,16 +1,30 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.sendfile('index.html');
+
+//utile pour permettre à nos page HTML de
+//charger des ressources (JavaScript, CSS,...)
+app.use(express.static(__dirname + '/'));
+
+
+//route pas défaut qui redirige vers l'annonce
+app.get('/:id', function (req, res) {
+
+  //on recupére l'id de l'annonce rechercher
+  var idAnnonce = req.params.id
+
+  //on redirige vers page avec les infos
+  req.query.id = idAnnonce;
+  //console.log(req.route);
+  res.sendFile(__dirname+'/html/index.html?id=1');
 })
 
 
 var server = app.listen(3000, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+  var host = server.address().address;
+  var port = server.address().port;
 
-  console.log('Server start and listen on http://%s:%s', host, port)
+  console.log('Server is starting and listen on http://%s:%s', host, port);
 
 })
