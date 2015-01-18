075e0b5c417c5c23dd8df4876ffd09bde1653e11
Cyril Dupont <cyril.dupont61@gmail.com>
2015-01-18 19:23:01 +0100

    ./cmd_utile.sh

--- cmd_utile.sh
+++ cmd_utile.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-git add --all
-git commit -m $0
-git push
-git push heroku master
-
+if [$# -eq 0]
+then
+    echo "pas de message de commit"
+else
+    git add --all
+    git commit -m $0
+    git push
+    git push heroku master
+fi
