3b12cc782b9f60f81e2b30f5b4ca5109a108b603
Cyril Dupont <cyril.dupont61@gmail.com>
2015-02-12 22:06:02 +0100

    maj access listeFlats

--- cmd_utile.sh
+++ cmd_utile.sh
@@ -2,13 +2,11 @@
 
 if [ -n "$1" ]
 then
-echo $1
   echo "*******************************"
   echo "*****   BEGING UPDATE    ******"
   echo "*******************************"
 
-  git add --all
-
+  git add -A
   git commit -m $1
   git push
   git push heroku master
