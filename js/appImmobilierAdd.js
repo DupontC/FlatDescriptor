42fc95ff147893ae29ba8cfef561885aa662e594
DupontC <cyril.dupont61@gmail.com>
2015-03-14 22:35:15 +0100

    ajout de controle formulaire phase 1

--- js/appImmobilierAdd.js
+++ js/appImmobilierAdd.js
@@ -11,23 +11,27 @@ FlatAppBack.controller('FlatBackOfficeCtrl',function($scope, $http, $location) {
 
   //function call to update data
   $scope.addData = function() {
-    console.log("ajout d'une annonce");
-    $scope.appartement.id_annonce =  Math.floor((Math.random() * 9999) + 2);
-    var jdata = 'majData='+JSON.stringify($scope.appartement); // The data is to be string.
-    console.log(jdata);
-    $http({ // Accessing the Angular $http Service to send data via REST Communication to Node Server.
-      method: "post",
-      url: urlInfo,
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-      data:  jdata
-    }).success(function(response) {
-      console.log("success"); // Getting Success Response in Callback
-      toastr.success("Annonce ajouté !");
+    if($scope.myForm.$valid){
+      console.log("ajout d'une annonce");
+      $scope.appartement.id_annonce =  Math.floor((Math.random() * 9999) + 2);
+      var jdata = 'majData='+JSON.stringify($scope.appartement); // The data is to be string.
+      console.log(jdata);
+      $http({ // Accessing the Angular $http Service to send data via REST Communication to Node Server.
+        method: "post",
+        url: urlInfo,
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        data:  jdata
+      }).success(function(response) {
+        console.log("success"); // Getting Success Response in Callback
+        toastr.success("Annonce ajouté !");
 
-    }).error(function(response) {
-      console.log("maj ko "+$scope.codeStatus);
-      toastr.error("Erreur lors de l'insertion!");
-    });//END HTTP
+      }).error(function(response) {
+        console.log("maj ko "+$scope.codeStatus);
+        toastr.error("Erreur lors de l'insertion!");
+      });//END HTTP
+    }else{
+      toastr.warning("Fromulaire de l'annonce invalide");
+    }
   };//END MAJDATA FUNCTION
 
 });//END CONTROLER
