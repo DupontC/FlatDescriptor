d293d8ec3244740cdf1535fe82da4b2bf82046b0
DupontC <cyril.dupont61@gmail.com>
2016-02-02 18:49:55 +0100

    post pour l'ajout d'un user 	modified:   serveurNodeJS.js

--- serveurNodeJS.js
+++ serveurNodeJS.js
@@ -377,6 +377,34 @@ app.get('/user/:id', function (req, res) {
   }
 });
 
+
+//web service qui maj un user lors des appels ajax
+app.post('/user/:id', function (req, res) {
+
+  var userAdd =  JSON.parse(req.body.majData);
+  if(req.session_state.username && null !== userAdd){
+    logger.debug(JSON.stringify(userAdd));
+
+    // Use the schema to register a model with MongoDb
+    mongoose.model('flat', flatSchema);
+    var flat = mongoose.model('flat');
+    var newAppart = new flat(flatAdd);
+    newAppart.save(function(err){
+      if(err){
+        logger.error("erreur lors de la m-a-j de l'annonce "+indice);
+        res.status(424).send("Erreur : add annonce ");
+      }else{
+        logger.debug("ajout d'une annonce ");
+        res.status(200).send("add annonce");
+      }
+    });
+
+  }else{
+    logger.debug("add annonce non permise");
+    res.status(401).send("Erreur : add non permise");
+  }
+});
+
 /**
 * web service qui retourne les informations
 *de tout les appartements en ligne lors des appels ajax
